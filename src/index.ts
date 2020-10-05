8b475a28dc22f09db442b0167b9ba131df389b64
attack-monkey <bclark.2111@gmail.com>
2020-10-06 05:02:13 +1030

    export LeanState and adjust some of the functions

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,8 @@ const setState_ = <S>(state, subs) => (stateKey: keyof S, stateAtKey: S[keyof S]
   })
 }
 
-const fromStateOnce_ = <S, SK>(state, subs) => <K extends keyof S>(stateKeys: K[], fn: (state: S) => any) => {
-  const state_ = state as S
-  fn(state_)
+const fromStateOnce_ = <S>(state) => (fn: (state: S) => any) => {
+  fn(state)
 }
 
 const fromState_ = <S, SK>(state, subs) => <K extends keyof S>(subKey: SK, stateKeys: K[], fn: (state: S) => any) => {
@@ -25,8 +24,7 @@ const fromState_ = <S, SK>(state, subs) => <K extends keyof S>(subKey: SK, state
   fn(state_)
 }
 
-const fromNextState_ = <S, SK>(state, subs) => <K extends keyof S>(subKey: SK, stateKeys: K[], fn: (state: S) => any) => {
-  const state_ = state as S
+const fromNextState_ = <S, SK>(subs) => <K extends keyof S>(subKey: SK, stateKeys: K[], fn: (state: S) => any) => {
   const subs_ = subs as Subscriptions<S>
   stateKeys.forEach(stateKey => {
     subs_[stateKey as string] = subs_[stateKey as string] ? subs_[stateKey as string] : {}
@@ -70,12 +68,21 @@ export const register = <S, SK>() => {
   let subs: any = {}
   return {
     setState: setState_<S>(state, subs),
-    fromStateOnce: fromStateOnce_<S, SK>(state, subs),
+    fromStateOnce: fromStateOnce_<S>(state),
     fromState: fromState_<S, SK>(state, subs),
     fromStateWhile: fromStateWhile_<S, SK>(state, subs),
-    fromNextState: fromNextState_<S, SK>(state, subs),
+    fromNextState: fromNextState_<S, SK>(subs),
     fromNextStateWhile: fromNextStateWhile_<S, SK>(state, subs)
   }
+}
+
+export type LeanState<S, SK> = {
+  setState: (state, subs) => (stateKey: keyof S, stateAtKey: S[keyof S]) => void
+  fromStateOnce: (state) => <K extends keyof S>(stateKeys: K[], fn: (state: S) => any) => void
+  fromState: (state, subs) => <K extends keyof S>(subKey: SK, stateKeys: K[], fn: (state: S) => any) => void
+  fromStateWhile: (state, subs) => <K extends keyof S>(subKey: SK, condition: (state: S) => boolean, stateKeys: K[], fn: (state: S) => any) => void
+  fromNextState: (subs) => <K extends keyof S>(subKey: SK, stateKeys: K[], fn: (state: S) => any) => void
+  fromNextStateWhile: (state, subs) => <K extends keyof S>(subKey: SK, condition: (state: S) => boolean, stateKeys: K[], fn: (state: S) => any) => void
 }
 
 
